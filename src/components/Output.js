065e5a94e9230b81df9d562199fdd85f365b4413
Venkat <v-vsrinivasa@expedia.com>
2020-06-02 08:52:25 +0530

    first commit

---
+++ src/components/Output.js
@@ -0,0 +1,18 @@
+
+import React, {Component} from 'react';
+
+
+export default class Keypad extends Component {
+
+  render() {
+    return (
+      <div className="result">
+        <b> {this.props.result} </b>
+          </div>
+    );
+  }
+}
+
+
+
+
